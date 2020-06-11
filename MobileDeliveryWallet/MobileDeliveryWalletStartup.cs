cbb99fc9e48a4071e5bbafe2090781325ecc707d
Ed Vergara <evergara@unitedwindowmfg.com>
2020-06-11 09:14:07 -0400

    SQLQueries

---
+++ MobileDeliveryWallet/MobileDeliveryWalletStartup.cs
@@ -0,0 +1,40 @@
+﻿using MobileDeliveryGeneral.Interfaces;
+using MobileDeliveryGeneral.Settings;
+using MobileDeliveryLogger;
+using MobileDeliveryServer;
+using MobileDeliveryWallet.WalletManagement;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MobileDeliveryWallet
+{
+    class MobileDeliveryWalletStartup
+    {
+        static Logger logger;
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            var config = WinformReadSettings.GetSettings(typeof(MobileDeliveryWalletStartup));
+
+            logger = new Logger(config.AppName + "_" + config.srvSet.port.ToString(), config.LogPath, config.LogLevel);
+            Logger.Info($"Starting {config.AppName} {config.srvSet.port.ToString()} {config.Version} {DateTime.Now}");
+            Logger.Info($"Logfile: {config.LogPath}");
+
+            Console.CancelKeyPress += (sender, eArgs) => {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
+            MobileDeliveryWalletAPI det = new MobileDeliveryWalletAPI();
+            Task.Run(() => { det.Init(config); });
+
+            Logger.Info($"Connection details {config.AppName}:/n/tUrl:/t{config.srvSet.url}/n/tPort:/t{config.srvSet.port}");
+           
+            ProcessMsgDelegateRXRaw pmRx = new ProcessMsgDelegateRXRaw(det.HandleClientCmd);
+            
+            // kick off asynchronous stuff 
+            _quitEvent.WaitOne();
+        }
+    }
+}
---
+++ MobileDeliveryWallet/WalletManagement/WalletServer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using MobileDeliveryLogger;
+using MobileDeliveryGeneral.Data;
+using MobileDeliveryGeneral.Interfaces;
+using MobileDeliveryGeneral.Interfaces.DataInterfaces;
+using MobileDeliveryGeneral.Threading;
+using static MobileDeliveryGeneral.Definitions.enums;
+using static MobileDeliveryGeneral.Definitions.MsgTypes;
+using System.Threading.Tasks;
+using MobileDeliveryClient.API;
+using MobileDeliveryGeneral.Settings;
+
+namespace MobileDeliveryWallet.WalletManagement
+{
+    public class WalletServer
+    {
+        //static GETH BlockchainBPIDData;
+        //static UMDDB UMD_Data;
+        ClientToServerConnection conn;
+        string cnn;
+        DateTime dt;
+        SendMsgDelegate sm;
+        ReceiveMsgDelegate rm;
+
+        #region collections
+          public List<WalletData> manifestMasterData = new List<WalletData>();
+        #endregion
+
+        #region backgroundworkers
+        UMBackgroundWorker<WalletData> walletData;
+
+        #endregion
+
+        #region progressupdates
+        UMBackgroundWorker<WalletData>.ProgressChanged<WalletData> pcWallet;
+        #endregion
+
+        public WalletServer(UMDAppConfig config)
+        {
+            Logger.Info($"WalletServer::Websocket connection: {config.srvSet.srvurl}");
+            
+            conn = new ClientToServerConnection(
+                config.srvSet, ref sm, rm);
+            conn.Connect();
+        }
+
+        public double SubscribeGetNewHeads() {
+
+            Logger.Debug("GetNewHeadds");
+            conn.SendCommandToServer(new Command() { command = eCommand.GetNewHeads });
+            
+            return 0.0;
+        }
+        public IMDMMessage QueryData(Func<byte[], Task> cb, isaCommand dat)
+        {
+            return null;
+        }
+
+
+        public IEnumerable<IMDMMessage> Persist(SPCmds sp, IMDMMessage md)
+        {
+            Logger.Info($"Persist {md.ToString()}");
+           // foreach (var it in UMD_Data.InsertData(sp, md))
+                yield return null;
+        }
+    
+    }
+}
