8d35126106e83ff273607d9121d1ce79984c9b96
Ed Vergara <evergara@unitedwindowmfg.com>
2020-06-26 12:13:57 -0400

    this checkout is incorrect, old code.

---
+++ MobileDeliveryUI/MobileDeliveryUI/Pages/ManifestDetails.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MobileDeliveryGeneral.Data;
+using MobileDeliveryGeneral.Interfaces.DataInterfaces;
+using MobileDeliveryMVVM.ViewModel;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileDeliveryUniversal.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManifestDetails : ContentPage
+    {
+        public ManifestDetails(IMDMMessage mmd)
+        {
+            string TRK_CDE = ((ManifestMasterData)mmd).TRK_CDE;
+            DateTime SHIP_DTE = ((ManifestMasterData)mmd).SHIP_DTE;
+            InitializeComponent();
+      
+
+            //var mvm = new ManifestVM();
+            //BindingContext = mvm;
+        }
+        public ManifestDetails()
+        {
+            InitializeComponent();
+            //var mvm = new ManifestVM();
+            //BindingContext = mvm;
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            //Navigation.PushAsync();
+        }
+
+        private void Trucks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StopList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
